3572ee809c5e32e62996db54d60bbb5dc4fb0497
chernsian <39767797+chernsian@users.noreply.github.com>
2019-01-29 22:08:20 +0800

    Initial Commit

---
+++ SmartCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartCamera
+//
+//  Created by Cheong Chern Sian on 29/01/2019.
+//  Copyright © 2019 Cheong Chern Sian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
